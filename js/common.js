d6cd22b21a300b72b3dcde53f1a086db6f9a64d4
jaehyanggg <jaehyanggg@gmail.com>
2021-07-19 13:59:40 +0900

    main

--- js/common.js
+++ js/common.js
@@ -1,119 +1,128 @@
-/*************** 글로벌 설정 *****************/
-var isHeaderAni = false
-initCommon()
+/*************** header-wrapper *****************/
+// 함수안에 넣어줌으로써 변수명 길게 안해도 됨 -> 즉, init 함수 밖에서 또 다르게 추가해도 됨
+$(function(){
 
-/*************** 사용자 함수 *****************/
-function initCommon() {
-	if($.cookie('hideNotice') === 'Y') onCloseNotice()
-	else {
-		$('.notice-wrapper').find('.notice-content').hide()
+	/*************** 글로벌 설정 *****************/
+	init()
+	
+	/*************** 사용자 함수 *****************/
+	function init() {
+		if($.cookie('hideNotice') === 'Y') onCloseNotice()
+		else {
+			$('.notice-wrapper').find('.notice-content').hide()
+			$('.notice-wrapper').find('.bt-hide').hide()
+		}
+	}
+	
+	/******* onScroll *******/
+	function scrollNotice(scTop) {
+		/* 쓰일것같은 변수 먼저 설정 */
+		var $notice	= $('.notice-wrapper')
+		var $link = $('.link-wrapper')
+		var $header = $('.header-wrapper')
+	
+		if(scTop == 0) {
+			if($.cookie('hideNotice') !== 'Y') $notice.show()
+			$link.show()
+			$header.css('top', 'unset')
+			$header.removeClass('active')
+		}
+		/* else if(scTop < 150) {
+			$notice.hide()
+			$link.hide()
+			$header.css('top', 'unset')
+			$header.removeClass('active')
+		} */
+		else {
+			$notice.hide()
+			$link.hide()
+			headerHeight = $header.outerHeight()
+			$header.css('top', -headerHeight + 'px')
+			$header.css('top') /* common.css - .header-wrapper{transition: top .35s} 적용하려고 */
+			$header.css('top', 0)
+			$header.addClass('active')
+		}
+	}
+	
+	
+	/*************** 이벤트 등록 *****************/
+	$(window).scroll(onScroll).trigger('scroll')
+	
+	$('.header-wrapper .navi').mouseenter(onNaviEnter)
+	$('.header-wrapper .navi').mouseleave(onNaviLeave)
+	
+	$('.notice-wrapper .bt-show').click(onShowNotice)
+	$('.notice-wrapper .bt-hide').click(onHideNotice)
+	$('.notice-wrapper .bt-close').click(onCloseNotice)
+	$('.notice-wrapper .bt-today').click(onHideTodayNotice)
+	
+	$('.header-wrapper .link-lang').click(onToggleLang)
+	$('.header-wrapper .link-lang').mouseenter(onShowLang)
+	$('.header-wrapper .link-lang').mouseleave(onHideLang)
+	$('.header-wrapper .link-lang .lang').click(onChgLang)
+	
+	
+	/*************** 이벤트 콜백 *****************/
+	function onNaviEnter() {
+		$('.header-wrapper .sub-wrapper').hide()
+		$(this).find('.sub-wrapper').show()
+		$('.header-wrapper .navi').removeClass('active')
+		$(this).addClass('active')
+	}
+	
+	function onNaviLeave() {
+		$('.header-wrapper .sub-wrapper').hide()
+		$('.header-wrapper .navi').removeClass('active')
+	}
+	
+	function onScroll(e) {
+		var scTop = $(this).scrollTop()
+		scrollNotice(scTop)
+	}
+	
+	function onShowNotice() {
+		$('.notice-wrapper').find('.bt-show').hide()
+		$('.notice-wrapper').find('.bt-hide').show()
+		$('.notice-wrapper').find('.notice-content').show()
+	}
+	
+	function onHideNotice() {
+		$('.notice-wrapper').find('.bt-show').show()
 		$('.notice-wrapper').find('.bt-hide').hide()
+		$('.notice-wrapper').find('.notice-content').hide()
 	}
-}
-
-/******* onScroll *******/
-function scrollNotice(scTop) {
-	/* 쓰일것같은 변수 먼저 설정 */
-	var $notice	= $('.notice-wrapper')
-	var $link = $('.link-wrapper')
-	var $header = $('.header-wrapper')
-
-	if(scTop == 0) {
-		if($.cookie('hideNotice') !== 'Y') $notice.show()
-		$link.show()
-		$header.css('top', 'unset')
-		$header.removeClass('active')
+	
+	function onCloseNotice() {
+		$('.notice-wrapper').hide()
 	}
-	/* else if(scTop < 150) {
-		$notice.hide()
-		$link.hide()
-		$header.css('top', 'unset')
-		$header.removeClass('active')
-	} */
-	else {
-		$notice.hide()
-		$link.hide()
-		headerHeight = $header.outerHeight()
-		$header.css('top', -headerHeight + 'px')
-		$header.css('top') /* common.css - .header-wrapper{transition: top .35s} 적용하려고 */
-		$header.css('top', 0)
-		$header.addClass('active')
+	
+	function onHideTodayNotice() {
+		$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
+		onCloseNotice()
 	}
-}
-
-
-/*************** 이벤트 등록 *****************/
-$(window).scroll(onScroll).trigger('scroll')
-
-$('.header-wrapper .navi').mouseenter(onNaviEnter)
-$('.header-wrapper .navi').mouseleave(onNaviLeave)
-
-$('.notice-wrapper .bt-show').click(onShowNotice)
-$('.notice-wrapper .bt-hide').click(onHideNotice)
-$('.notice-wrapper .bt-close').click(onCloseNotice)
-$('.notice-wrapper .bt-today').click(onHideTodayNotice)
-
-$('.header-wrapper .link-lang').click(onToggleLang)
-$('.header-wrapper .link-lang').mouseenter(onShowLang)
-$('.header-wrapper .link-lang').mouseleave(onHideLang)
-$('.header-wrapper .link-lang .lang').click(onChgLang)
-
-
-/*************** 이벤트 콜백 *****************/
-function onNaviEnter() {
-	$('.header-wrapper .sub-wrapper').hide()
-	$(this).find('.sub-wrapper').show()
-	$('.header-wrapper .navi').removeClass('active')
-	$(this).addClass('active')
-}
-
-function onNaviLeave() {
-	$('.header-wrapper .sub-wrapper').hide()
-	$('.header-wrapper .navi').removeClass('active')
-}
-
-function onScroll(e) {
-	var scTop = $(this).scrollTop()
-	scrollNotice(scTop)
-}
-
-function onShowNotice() {
-	$('.notice-wrapper').find('.bt-show').hide()
-	$('.notice-wrapper').find('.bt-hide').show()
-	$('.notice-wrapper').find('.notice-content').show()
-}
-
-function onHideNotice() {
-	$('.notice-wrapper').find('.bt-show').show()
-	$('.notice-wrapper').find('.bt-hide').hide()
-	$('.notice-wrapper').find('.notice-content').hide()
-}
-
-function onCloseNotice() {
-	$('.notice-wrapper').hide()
-}
-
-function onHideTodayNotice() {
-	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
-	onCloseNotice()
-}
-
-function onToggleLang() {
-	$('.header-wrapper .link-lang .hover').toggle()
-}
-
-function onShowLang() {
-	$('.header-wrapper .link-lang .hover').show()
-}
-
-function onHideLang() {
-	$('.header-wrapper .link-lang .hover').hide()
-}
-
-function onChgLang() {
-	var $span = $(this).parent().prev().find('span')
-	var myLang = $(this).text()
-	var spanLang = $span.text()
-	$span.text(myLang)
-	$(this).text(spanLang)
-}
+	
+	function onToggleLang() {
+		$('.header-wrapper .link-lang .hover').toggle()
+	}
+	
+	function onShowLang() {
+		$('.header-wrapper .link-lang .hover').show()
+	}
+	
+	function onHideLang() {
+		$('.header-wrapper .link-lang .hover').hide()
+	}
+	
+	function onChgLang() {
+		var $span = $(this).parent().prev().find('span')
+		var myLang = $(this).text()
+		var spanLang = $span.text()
+		$span.text(myLang)
+		$(this).text(spanLang)
+	}
+	
+	
+	
+	
+	})
+	
---
+++ js/index.js
@@ -0,0 +1,59 @@
+// main-wrapper에서 할일
+// 1. 배너가 자동으로 움직인다
+// 2. 배너가 나타나면 그 후에 내부요소들이 animation으로 나타난다
+// 3. 동영상은 플레이가 끝난 이후에 다음으로 넘어간다
+
+
+/*************** main-wrapper *****************/
+// 함수안에 넣어줌으로써 변수명 길게 안해도 됨 -> 즉, init 함수 밖에서 또 다르게 추가해도 됨
+$(function(){
+
+	/*************** 글로벌 설정 *****************/
+	var $wrapper = $('.main-wrapper')
+	var $slide = $('.main-wrapper .slide')
+	var video = $('.main-wrapper .video')[0]
+	var len = $slide.length
+	var lastIdx = len - 1
+	var depth = 2
+	var idx = 0
+	var gap = 3000
+	var speed = 500
+	// var interval
+	init()
+	
+	/*************** 사용자 함수 *****************/
+	function init() {
+		$slide.eq(idx).css('z-index', depth++)
+		$slide.eq(idx).addClass('active')
+		onAni()
+	}
+	
+	/*************** 이벤트 등록 *****************/
+	video.addEventListener('loadeddata', onLoadedVideo)
+	video.addEventListener('ended', onPlay)
+	
+	/*************** 이벤트 콜백 *****************/
+	function onLoadedVideo() {
+		if(video.readyState >= 2) {
+			video.playbackRate = 4.0
+		}
+	}
+	
+	function onAni() {
+		$(this).addClass('active')
+		video.currentTime = 0
+			if($slide.eq(idx).hasClass('is-video')) video.play()
+			else setTimeout(onPlay, gap)
+	}
+	
+	function onPlay() {
+		idx = (idx == lastIdx) ? 0 : idx + 1
+		$slide.eq(idx).css({'z-index': depth++, 'left': '100%'})
+		$slide.removeClass('active')
+		$slide.eq(idx).stop().animate({'left': 0}, speed, onAni)
+	}
+	
+	
+	
+	
+	})
