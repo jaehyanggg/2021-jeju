6959f7017311b3fefe35cf345a4c7194aa05f653
jaehyanggg <jaehyanggg@gmail.com>
2021-07-20 16:12:48 +0900

    promo

--- js/index.js
+++ js/index.js
@@ -6,182 +6,188 @@
 /* if(idx == lastIdx) mainIdx = 0
 else idx = idx + 1 */
 
-/*************** main-wrapper *****************/
+/* for(var i=0; i<len; i++) {
+	$('<i class="pager"></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active': '')
+} */
+
+/*************** Index *****************/
 $(function () {
 
-	/*************** 글로벌 설정 *****************/
-	var $wrapper = $('.main-wrapper')
-	var $slide = $('.main-wrapper .slide')
-	var $pagerSlide = $('.main-wrapper .pager-slide')
-	var video = $('.main-wrapper .video')[0]
-	var $weather = $('.main-wrapper .weather')
-	var len = $slide.length
-	var lastIdx = len - 1
-	var depth = 2
-	var idx = 0
-	var gap = 5000
-	var speed = 500
-	var timeout
-	var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
-	var weatherData = {
-		appid: '02efdd64bdc14b279bc91d9247db4722',
-		units: 'metric'
-	}
-	var weatherIcon = {
-		i01d: 'bi-brightness-high',
-		i01n: 'bi-brightness-high-fill',
-		i02d: 'bi-cloud-sun',
-		i02n: 'bi-cloud-sun-fill',
-		i03d: 'bi-cloud',
-		i03n: 'bi-cloud-fill',
-		i04d: 'bi-clouds',
-		i04n: 'bi-cloud-fills',
-		i09d: 'bi-cloud-rain-heavy',
-		i09n: 'bi-cloud-rain-heavy-fill',
-		i10d: 'bi-cloud-drizzle',
-		i10n: 'bi-cloud-drizzle-fill',
-		i11d: 'bi-cloud-lightning',
-		i11n: 'bi-cloud-lightning-fill',
-		i13d: 'bi-cloud-snow',
-		i13n: 'bi-cloud-snow-fill',
-		i50d: 'bi-cloud-haze',
-		i50n: 'bi-cloud-haze-fill',
-	}
-	init()
 	weather()
+	setCookie()
+	slideMain()
+	slideDream()
+	slidePromo()
 
-	/*************** 사용자 함수 *****************/
-	function init() {
-		if ($.cookie('hideCookie') === 'Y') onCloseCookie()
+	function setCookie() {
+		var $cookieWrapper = $('.cookie-wrapper')
+		var $cookieClose = $cookieWrapper.find('.bt-close')
+		var $cookieConfirm = $cookieWrapper.find('.bt-confirm')
+
+		if($.cookie('hideCookie') === 'Y') onCloseCookie()
+
+		function onCloseCookie() {
+			$('.cookie-wrapper').hide()
+		}
+
+		function onCloseTodayCookie() {
+			$.cookie('hideCookie', 'Y', {
+				expires: 1,
+				path: '/'
+			})
+			onCloseCookie()
+		}
+
+		$cookieClose.click(onCloseCookie)
+		$cookieConfirm.click(onCloseTodayCookie)
+	}
+
+	function slideMain() {
+		var $slide = $('.main-wrapper .slide')
+		var $pagerSlide = $('.main-wrapper .pager-slide')
+		var video = $('.main-wrapper .video')[0]
+		var len = $slide.length
+		var lastIdx = len - 1
+		var depth = 2
+		var idx = 0
+		var gap = 5000
+		var speed = 500
+		var timeout
+
+		function onPagerClick() {
+			idx = $(this).index()
+			onPlay('pager')
+		}
+
+		function onModalVideo() {
+			$('.modal-video').show()
+		}
+	
+		function onModalVideoClose() {
+			$('.modal-video').hide()
+		}
+	
+		function onLoadedVideo() {
+			if (video.readyState >= 2) {
+				video.playbackRate = 4.0
+			}
+		}
+	
+		function ani() {
+			$(this).addClass('active')
+			video.currentTime = 0
+			if ($slide.eq(idx).hasClass('is-video')) video.play()
+			else {
+				clearTimeout(timeout)
+				timeout = setTimeout(onPlay, gap)
+			}
+		}
+	
+		function onPlay(e) {
+			if (e !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1
+			$pagerSlide.find('.pager').removeClass('active')
+			$pagerSlide.find('.pager').eq(idx).addClass('active')
+			$slide.eq(idx).css({
+				'z-index': depth++,
+				'left': '100%'
+			})
+			$slide.removeClass('active')
+			$slide.eq(idx).stop().animate({
+				'left': 0
+			}, speed, ani)
+		}
+
 		$slide.eq(idx).css('z-index', depth++)
 		$slide.eq(idx).addClass('active')
-
 		for (var i = 0; i < len; i++) $pagerSlide.append('<i class="pager"></i>')
 		$pagerSlide.find('.pager').click(onPagerClick)
 		$pagerSlide.find('.pager').eq(idx).addClass('active')
+		video.addEventListener('loadeddata', onLoadedVideo)
+		video.addEventListener('ended', onPlay)
+		$('.bt-video').click(onModalVideo)
+		$('.modal-video').find('.bt-close').click(onModalVideoClose)
 
-		/* for(var i=0; i<len; i++) {
-			$('<i class="pager"></i>').appendTo($pagerSlide).click(onPagerClick).addClass((idx === i) ? 'active': '')
-		} */
 		ani()
-		slideDream()
 	}
 
 	function weather() {
+		var $weather = $('.main-wrapper .weather')
+		var weatherURL = 'https://api.openweathermap.org/data/2.5/weather'
+		var weatherData = {
+			appid: '02efdd64bdc14b279bc91d9247db4722',
+			units: 'metric'
+		}
+		var weatherIcon = {
+			i01d: 'bi-brightness-high',
+			i01n: 'bi-brightness-high-fill',
+			i02d: 'bi-cloud-sun',
+			i02n: 'bi-cloud-sun-fill',
+			i03d: 'bi-cloud',
+			i03n: 'bi-cloud-fill',
+			i04d: 'bi-clouds',
+			i04n: 'bi-cloud-fills',
+			i09d: 'bi-cloud-rain-heavy',
+			i09n: 'bi-cloud-rain-heavy-fill',
+			i10d: 'bi-cloud-drizzle',
+			i10n: 'bi-cloud-drizzle-fill',
+			i11d: 'bi-cloud-lightning',
+			i11n: 'bi-cloud-lightning-fill',
+			i13d: 'bi-cloud-snow',
+			i13n: 'bi-cloud-snow-fill',
+			i50d: 'bi-cloud-haze',
+			i50n: 'bi-cloud-haze-fill',
+		}
+
+		function onGetWeather(r) {
+			console.log(r)
+			$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
+			$weather.find('.temp').text(r.main.temp)
+			$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
+			$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
+			$weather.find('.time > small').text(moment(r.dt * 1000).format('A'))
+		}
+	
+		function onGetGeo(r) {
+			weatherData.lat = r.coords.latitude
+			weatherData.lon = r.coords.longitude
+			$.get(weatherURL, weatherData, onGetWeather)
+		}
+	
+		function onErrorGeo() {
+			weatherData.lat = 33.485739737138786
+			weatherData.lon = 126.48154043372092
+			$.get(weatherURL, weatherData, onGetWeather)
+		}
 		// 위치정보 가져오기(못 가져오면 제주도 보이기 33.485739737138786, 126.48154043372092)
 		navigator.geolocation.getCurrentPosition(onGetGeo, onErrorGeo)
 	}
 
 	function slideDream() {
-		var swiper = new Swiper('.dream-wrapper .swiper-container', {
-			pagination: {
-				el: '.dream-wrapper .pager-wrapper',
-				clickable: true
-			},
-			navigation: {
-				nextEl: '.dream-wrapper .bt-slide.right',
-				prevEl: '.dream-wrapper .bt-slide.left',
-			},
-			loop: true,
-			slidesPerView: 1,
-			spaceBetween: 40,
-			breakpoints: {
-				576: {
-					slidesPerView: 2,
-				},
-				992: {
-					slidesPerView: 3,
-				}
-			}
-		});
+		var swiper = getSwiper('.dream-wrapper', { break: 3 })
 	}
 
+	function slidePromo() {
+		var $promoWrapper = $('.promo-wrapper')
+		var $slideWrap = $promoWrapper.find('.slide-wrap')
 
-	/*************** 이벤트 등록 *****************/
-	video.addEventListener('loadeddata', onLoadedVideo)
-	video.addEventListener('ended', onPlay)
-	$('.bt-video').click(onModalVideo)
-	$('.modal-video').find('.bt-close').click(onModalVideoClose)
-	$('.cookie-wrapper').find('.bt-close').click(onCloseCookie)
-	$('.cookie-wrapper').find('.bt-confirm').click(onCloseTodayCookie)
-
-
-
-	/*************** 이벤트 콜백 *****************/
-	function onCloseTodayCookie() {
-		$.cookie('hideCookie', 'Y', {
-			expires: 1,
-			path: '/'
-		})
-		onCloseCookie()
-	}
-
-	function onGetWeather(r) {
-		console.log(r)
-		$weather.find('.icon').addClass(weatherIcon['i' + r.weather[0].icon])
-		$weather.find('.temp').text(r.main.temp)
-		$weather.find('.date').text(moment(r.dt * 1000).format('YYYY. M. D. ddd'))
-		$weather.find('.time > span').text(moment(r.dt * 1000).format('hh:mm'))
-		$weather.find('.time > small').text(moment(r.dt * 1000).format('A'))
-	}
-
-	function onGetGeo(r) {
-		weatherData.lat = r.coords.latitude
-		weatherData.lon = r.coords.longitude
-		$.get(weatherURL, weatherData, onGetWeather)
-	}
-
-	function onErrorGeo() {
-		weatherData.lat = 33.485739737138786
-		weatherData.lon = 126.48154043372092
-		$.get(weatherURL, weatherData, onGetWeather)
-	}
-
-	function onPagerClick() {
-		idx = $(this).index()
-		onPlay('pager')
-	}
-
-	function onCloseCookie() {
-		$('.cookie-wrapper').hide()
-	}
-
-	function onModalVideo() {
-		$('.modal-video').show()
-	}
-
-	function onModalVideoClose() {
-		$('.modal-video').hide()
-	}
-
-	function onLoadedVideo() {
-		if (video.readyState >= 2) {
-			video.playbackRate = 4.0
+		function onGetData(r) {
+			// for(var i=0; i<r.promo.length; i++) {}
+			r.promo.forEach(function(v, i) {
+				var html = ''
+				html += '<li class="slide swiper-slide">'
+				html += '<div class="img-wrap ratio" data-ratio="1">'
+				html += '<div class="ratio-bg" style="background-image: url('+v.src+');"></div>'
+				html += '</div>'
+				html += '<div class="cont-wrap">'
+				html += '<h3 class="title">'+v.title+'</h3>'
+				html += '<div class="desc">'+v.desc+'</div>'
+				html += '</div>'
+				html += '</li>'
+				$slideWrap.append(html)
+			})
+			var swiper = getSwiper('.promo-wrapper', { break: 4, pager: false });
 		}
-	}
-
-	function ani() {
-		$(this).addClass('active')
-		video.currentTime = 0
-		if ($slide.eq(idx).hasClass('is-video')) video.play()
-		else {
-			clearTimeout(timeout)
-			timeout = setTimeout(onPlay, gap)
-		}
-	}
-
-	function onPlay(e) {
-		if (e !== 'pager') idx = (idx == lastIdx) ? 0 : idx + 1
-		$pagerSlide.find('.pager').removeClass('active')
-		$pagerSlide.find('.pager').eq(idx).addClass('active')
-		$slide.eq(idx).css({
-			'z-index': depth++,
-			'left': '100%'
-		})
-		$slide.removeClass('active')
-		$slide.eq(idx).stop().animate({
-			'left': 0
-		}, speed, ani)
+		$.get('../json/promotion.json', onGetData)	// init
 	}
 
 })
